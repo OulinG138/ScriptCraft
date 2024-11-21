e98e5ae3d769feb4853186518af86252f38dbd88
Anthony Wang <anthonywang@Anthonys-MacBook-Pro.local>
2024-11-21 04:18:01 -0500

    minor change to the folder name so the url sticks to kebab case

--- PP2/pages/api-docs.tsx
+++ PP2/pages/api-docs.tsx
@@ -1,6 +1,8 @@
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
-export default function ApiDocs() {
+const ApiDocs: React.FC = () => {
   return <SwaggerUI url="/api/swagger" />;
-}
+};
+
+export default ApiDocs;
